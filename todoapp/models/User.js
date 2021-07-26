502124f6d9e9de6f2ef9ad71f45a9ea0eefea620
bearjin <kwj5030@gmail.com>
2021-07-26 21:51:15 +0900

    Update: login/logout

--- todoapp/models/User.js
+++ todoapp/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -31,6 +34,60 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  // plainPassword (1234567) 과 암호화된 비밀번호가 맞는지 체크
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+}
+
+userSchema.methods.generateToken = function (callback) {
+  var user = this;
+
+  // jsonwebtoken을 이용해서 token을 생성
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  // user._id + 'secretToken' = token 
+  // -> 
+  // 'secretToken' -> user._id
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+}
+
+userSchema.statics.findByToken = function (token, callback) {
+  var user = this;
+
+  // 토큰을 decode 한다.
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return callback(err);
+      callback(null, user);
+    });
+  });
+}
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
